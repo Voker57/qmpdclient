b899d238761c1716b553d0f6d68841a7e6502f2e
Voker57 <voker57@gmail.com>
2009-01-12 22:45:21 +0300

    Licenses!

--- src/lastfmsubmitter.cpp
+++ src/lastfmsubmitter.cpp
@@ -1,3 +1,23 @@
+/*
+ * QMPDClient - An MPD client written in Qt 4.
+ * Copyright (C) 2005-2008 Håvard Tautra Knutsen <havtknut@tihlde.org>
+ * Copyright (C) 2009 Voker57 <voker57@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include "lastfmsubmitter.h"
 #include "config.h"
 
@@ -5,6 +25,7 @@
 #include <QNetworkRequest>
 #include <QNetworkReply>
 #include <QUrl>
+#include <QStringList>
 #include <QCryptographicHash>
 
 #include <QDebug>
